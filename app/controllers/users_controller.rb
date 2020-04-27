ae6581f1e8f3834e7b9846cf6cfce5b8eb44b496
makiharu <kitakazetoshirokuma@gmail.com>
2020-04-27 06:40:51 +0000

    カラム修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,7 @@ class UsersController < ApplicationController
 	end
 
 	def user_params
-		params.require(:user).permit(:name, :introduction)
+		params.require(:user).permit(:name, :introduction, :profile_image)
 	end
 
 end
